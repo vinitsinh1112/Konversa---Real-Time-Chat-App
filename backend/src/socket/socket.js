7181d386b5b634402089570dd93069e00145405d
Vinit Baria <bariavinit0912@gmail.com>
2026-07-26 20:09:08 +0530

    added last message time in chat list item

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -17,11 +17,7 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
 
-
-
-
     const userId = socket.handshake.query.userId;
-
 
     if (userId) {
         userSocketMap[userId] = socket.id;
